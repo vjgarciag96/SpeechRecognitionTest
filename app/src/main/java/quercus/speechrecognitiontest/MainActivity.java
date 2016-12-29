412346341844b1f27a5ce283ae1dbe58e4582f99
vjgarciag96 <vjgarciag96@gmail.com>
2016-12-29 18:15:49 +0100

    prueba

--- app/src/main/java/quercus/speechrecognitiontest/MainActivity.java
+++ app/src/main/java/quercus/speechrecognitiontest/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -139,6 +140,7 @@ public class MainActivity extends AppCompatActivity implements SpeechDelegate{
             Speech.getInstance().say("Repítelo, por favor", new TextToSpeechCallback() {
                 @Override
                 public void onStart() {
+                    Log.d("viti", "onStart: ");
                 }
 
                 @Override
